e554ad266af0d114c4b0b4278f10d7d56f540693
LeToyek <maulanatoyek@gmail.com>
2023-01-09 14:47:24 +0700

    update homepage

--- festiwise/resources/views/index.blade.php
+++ festiwise/resources/views/index.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.index')
-
 @section('container')
-    
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <header class="masthead">
+        <div class="container px-4 px-lg-5 h-100">
+            <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                <div class="col-lg-8 align-self-end">
+                    <h1 class="text-white font-weight-bold">Hold Events Easily With Festiwise</h1>
+                    <hr class="divider" />
+                </div>
+                <div class="col-lg-8 align-self-baseline">
+                    <p class="mb-5" style="color: darkgrey">Start Bootstrap can help you build better websites using the
+                        Bootstrap
+                        framework! Just download a theme and start customizing, no strings attached!</p>
+                    <div class="row justify-content-center">
+                        <button class="col-3">Get Started</button>
+                        <button class="col-3">Find Events</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 @endsection
